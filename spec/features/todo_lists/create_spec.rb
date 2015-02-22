ca6d15ee6616205fe75545afbfca1839be14f83a
Daniel Lopera <dlopera3@eafit.edu.co>
2015-02-21 22:05:28 -0500

    Refactor todo list creation in spec

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,24 +1,25 @@
 require 'spec_helper'
 describe "Creating todo lists" do
 
-  it "redirects to the todo list index page on succes" do
+  def create_todo_list(options={})
+    options[:title] ||= "My Todo list"
+    options[:description] ||= "This is what I'm doing today"
     visit "/todo_lists"
     click_link "New Todo list"
     expect(page).to have_content("New Todo List")
-    fill_in "Title", with: "My Todo list"
-    fill_in "Description", with: "This is what I'm doing today"
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
     click_button "Create Todo list"
+  end
+
+  it "redirects to the todo list index page on succes" do
+    create_todo_list
     expect(page).to have_content("My Todo list")
   end
 
 
   it "displays an error when title has less than 3 characters" do
-    visit "/todo_lists"
-    click_link "New Todo list"
-    expect(page).to have_content("New Todo List")
-    fill_in "Title", with: "Hi"
-    fill_in "Description", with: "This is what I'm doing today"
-    click_button "Create Todo list"
+    create_todo_list title:"Hi"
     expect(page).to have_content("error")
   end
 end
